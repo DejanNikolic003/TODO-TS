31ce2c3ae1ea3cb6edb9af48c43cdf029ce931c2
DejanNikolic003 <knolekjhnny@gmail.com>
2025-08-10 18:10:16 +0200

    Added showTasks function, to show in the dom.

--- src/models/tasks.ts
+++ src/models/tasks.ts
@@ -12,3 +12,23 @@ export const getAllTasks = async (): Promise<Item[]> => {
 
     return [];
 };
+
+export const showTasks = (tasks: Item[]): void => {
+    const taskList = document.getElementById("taskList") as HTMLDivElement;
+
+    tasks.forEach(task => {
+        const taskDiv: HTMLDivElement = document.createElement("div");
+        const taskHeading: HTMLHeadingElement = document.createElement("h2");
+        const taskDescription: HTMLParagraphElement = document.createElement("p");
+        const taskPriority: HTMLParagraphElement = document.createElement("p");
+        const taskDueDate: HTMLParagraphElement = document.createElement("p");
+
+        taskHeading.textContent = task.title;
+        taskDescription.textContent = task.description;
+        taskPriority.textContent = task.priority;
+        taskDueDate.textContent = task.due_date;        
+        
+        taskDiv.append(taskHeading, taskDescription, taskPriority, taskDueDate);
+        taskList.append(taskDiv);
+    });
+}
