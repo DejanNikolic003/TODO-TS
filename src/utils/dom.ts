19a15b062a3578fc589f6cdaab0a5932b08d4452
DejanNikolic003 <knolekjhnny@gmail.com>
2025-08-10 18:10:03 +0200

    Deleted file.

--- src/utils/dom.ts
+++
@@ -1,7 +0,0 @@
-export const createElement = (type: string, id: string, content: string): HTMLElement => {
-    const element = document.createElement(type);
-    element.id = id;
-    element.innerText = content;
-
-    return element;
-};
